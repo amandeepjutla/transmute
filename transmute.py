83fe27db362893c2227726235322b8cbaaa67807
Amandeep Jutla <ajutla@gmail.com>
2015-04-21 14:15:36 -0700

    structural changes

--- transmute.py
+++ transmute.py
@@ -4,19 +4,25 @@ import sqlite3
 # name, tradename, class, conversion-factor, typicaldose
 # Name, Trade, Class, CF, Typical
 
-connection = sqlite3.connect("drugs.sqlite")
+class Database(object):
+    def __init__(self, filename):
+        self.connection = sqlite3.connect(filename)
 
-with connection:
-    connection.row_factory = sqlite3.Row
+        with self.connection:
+            self.connection.row_factory = sqlite3.Row
+            self.cursor = self.connection.cursor()
+            self.cursor.execute("SELECT * FROM Drugs")
+            self.contents = self.cursor.fetchall()
 
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM Drugs")
+    def send_contents(self):
+        return self.contents
 
-    drugs = cursor.fetchall()
+data = Database("drugs.sqlite")
+drugs = data.send_contents()
 
-    for drug in drugs:
-        print drug["Name"]
-        print drug["Trade"]
-        print drug["Class"]
-        print drug["CF"]
-        print drug["Typical"]
+for drug in drugs:
+    print drug["Name"]
+    print drug["Trade"]
+    print drug["Class"]
+    print drug["CF"]
+    print drug["Typical"]
