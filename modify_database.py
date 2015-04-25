46e84873aad979e2ad25e5de108e48cc76478901
Amandeep Jutla <ajutla@gmail.com>
2015-04-24 20:45:12 -0700

    Added bz to database

--- modify_database.py
+++ modify_database.py
@@ -1,7 +1,7 @@
 import sqlite3
 import sys
 
-connection = sqlite3.connect("antipsychotics.sqlite")
+connection = sqlite3.connect("drugs.sqlite")
 
 q = """
         INSERT INTO Drugs(Name, Trade, Class, CF, Typical)
@@ -9,4 +9,5 @@ q = """
         """
 with connection:
     cursor = connection.cursor()
-    cursor.execute(q, ("name", "trade", "class", 0, 0))
+    cursor.execute("CREATE TABLE Benzodiazepines(Name TEXT, Trade TEXT, CF NUMERIC, PeakMin NUMERIC, PeakMax NUMERIC, HalfMin NUMERIC, HalfMax NUMERIC)")
+#    cursor.execute(q, ("name", "trade", "class", 0, 0))
