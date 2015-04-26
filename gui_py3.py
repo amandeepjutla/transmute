320241e3029c44cce4426550ab82ae59107d10aa
Amandeep Jutla <ajutla@gmail.com>
2015-04-25 18:56:07 -0700

    working gui

--- gui_py3.py
+++ gui_py3.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, RIGHT, LEFT, TOP, BOTTOM, BOTH, RAISED, END, SW, W, Listbox, StringVar
+from tkinter import Tk, RIGHT, LEFT, TOP, BOTTOM, BOTH, FLAT, RAISED, SUNKEN, RIDGE, END, SW, W, Listbox, StringVar, Toplevel
 from tkinter import ttk
 
 #ttk import Frame, Button, Style, Label
@@ -47,12 +47,12 @@ class Example(ttk.Frame):
         self.style = ttk.Style()
         self.style.theme_use("default")
 
-        box1 = Listbox(self)
-        box2 = Listbox(self)
+        box1 = Listbox(self, height=23)
+        box2 = Listbox(self, height=23)
 
-        antipsychotics = Database("drugs.sqlite", "ANTIPSYCHOTICS")
-        
-        dumped = antipsychotics.dump_drugs()
+        self.antipsychotics = Database("drugs.sqlite", "ANTIPSYCHOTICS")
+    
+        dumped = self.antipsychotics.dump_drugs()
 
         d = {}
         for i, row in enumerate(dumped):    
@@ -62,48 +62,82 @@ class Example(ttk.Frame):
             d[i] = l
 
         for drug in d.values():
-            drug = drug.replace("'","").replace("[","").replace("]","")
+            drug_string = str(drug)
+            drug_string = drug_string.replace("'","").replace("[","").replace("]","")
 
-            box1.insert(END, drug)
-            box2.insert(END, drug)
+            box1.insert(END, drug_string)
+            box2.insert(END, drug_string)
 
         box1.bind("<<ListboxSelect>>", self.onSelect1)
-        box1.pack(side=LEFT, padx=5, pady=5)
+        box1.pack(side=LEFT, padx=5, pady=3)
 
         box2.bind("<<ListboxSelect>>", self.onSelect2)
         box2.pack(side=LEFT)
 
-        frame = ttk.Frame(self, relief=RAISED, borderwidth=1)
-        frame.pack(fill=BOTH, expand=1)
         self.pack(fill=BOTH, expand=1, side=LEFT)
 
+        self.dose_entry = ttk.Entry(self, width=35)
+        self.dose_entry.pack(side=TOP, anchor=W, padx=5, pady=3)
+
         self.convert_from = StringVar()
-        self.convert_from.set("From: ")
-        self.label_from = ttk.Label(self, anchor=W, textvariable=self.convert_from)
-        self.label_from.pack(side=TOP, anchor=W, padx=5)
+        self.convert_from.set("mg of one antipsychotic roughly equates to:")
+        self.label_from = ttk.Label(self, anchor=W, textvariable=self.convert_from, width=35)
+        self.label_from.pack(side=TOP, anchor=W, padx=5, pady=3)
+
+        self.result_given = StringVar()
+        self.result_given.set("?")
+        self.result_label = ttk.Label(self, anchor=W, textvariable=self.result_given, width=35, relief=RAISED, background="white")
+        self.result_label.pack(side=TOP, anchor=W, padx=5, pady=3)
 
         self.convert_to = StringVar()
-        self.convert_to.set("To: ")
+        self.convert_to.set("mg of another antipsychotic.")
         self.label_to = ttk.Label(self, anchor=W, textvariable=self.convert_to)
-        self.label_to.pack(side=TOP, anchor=W, padx=5, pady=5)
+        self.label_to.pack(side=TOP, anchor=W, padx=5, pady=3)
 
-        okButton = ttk.Button(self, text="Exit")
-        okButton.pack(side=BOTTOM, padx=5, pady=5)
+        frame = ttk.Frame(self, relief=FLAT, borderwidth=1)
+        frame.pack(fill=BOTH, expand=1)
 
-        closeButton = ttk.Button(self, text="Convert")
-        closeButton.pack(side=BOTTOM)
+        convert_button = ttk.Button(self, text="Convert", command = self.convert)
+        convert_button.pack(side=LEFT, padx=5, pady=3)
+
+        exit_button = ttk.Button(self, text="Exit", command = self.kill)
+        exit_button.pack(side=LEFT, padx=5, pady=3)
+
+    def give_result(self, result):
+        toplevel = Toplevel()
+        frame = ttk.Frame(self, borderwidth=1)
+        frame.pack()
+
+        label1 = ttk.Label(toplevel, text=result)
+        label1.pack()
+
+    def convert(self):
+        db_from = self.antipsychotics.get_drug(from_drug)
+        db_to = self.antipsychotics.get_drug(to_drug)
+        dose = self.dose_entry.get()
+
+        multiplier = float(db_from["CF"])/float(db_to["CF"])
+        result = float(dose) * multiplier
+        self.result_given.set(result)
+
+#        self.give_result(result)
 
     def onSelect1(self, val):
         sender = val.widget
         idx = sender.curselection()
-        value = sender.get(idx)
-        self.convert_from.set("From: "+value)
+        global from_drug 
+        from_drug = sender.get(idx)
+        self.convert_from.set("mg of "+from_drug+" roughly equates to:")
 
     def onSelect2(self, val):
         sender = val.widget
         idx = sender.curselection()
-        value = sender.get(idx)
-        self.convert_to.set("To: "+value)
+        global to_drug
+        to_drug = sender.get(idx)
+        self.convert_to.set("mg of "+to_drug+".")
+
+    def kill(self):
+        self.quit()
 
 def main():
     root = Tk()
