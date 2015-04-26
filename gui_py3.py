181b63e2b7798358a065ea919666d7e2fbde8d42
Amandeep Jutla <ajutla@gmail.com>
2015-04-25 21:56:00 -0700

    refactored

--- gui_py3.py
+++ gui_py3.py
@@ -1,9 +1,8 @@
 #transmute: antipsychotic tool
 
-from tkinter import Tk, RIGHT, LEFT, TOP, BOTTOM, BOTH, FLAT, RAISED, SUNKEN, RIDGE, END, SW, W, Listbox, StringVar, Toplevel
-from tkinter import ttk
-
 import sqlite3
+import tkinter
+from tkinter import ttk
 
 class Database(object):
     def __init__(self, filename, table):
@@ -23,15 +22,6 @@ class Database(object):
             if query in drug:
                 return drug
 
-    def dict_gen(list):
-        d = {}
-        for i, row in enumerate(list):
-            l = []
-            for col in range(0, len(row)):
-                l.append(row[col]) 
-            d[i] = l
-        return d
-
     def dump_drugs(self):
         drug_list = []
         for drug in self.drugs:
@@ -43,66 +33,67 @@ class TransmuteAntipsychotics(ttk.Frame):
         ttk.Frame.__init__(self, parent)
         self.parent = parent
         self.initUI()
+
     def initUI(self):
         self.parent.title("Transmute: Antipsychotics")
         self.style = ttk.Style()
         self.style.theme_use("default")
 
-        box1 = Listbox(self, height=23)
-        box2 = Listbox(self, height=23)
+        from_box = tkinter.Listbox(self, height=23)
+        to_box = tkinter.Listbox(self, height=23)
 
         self.antipsychotics = Database("drugs.sqlite", "ANTIPSYCHOTICS")
     
         dumped = self.antipsychotics.dump_drugs()
 
-        d = {}
+        drug_dictionary = {}
         for i, row in enumerate(dumped):    
             l = []
             for col in range(0, len(row)):
                 l.append(row[col]) 
-            d[i] = l
+            drug_dictionary[i] = l
 
-        for drug in sorted(d.values()):
+        for drug in sorted(drug_dictionary.values()):
             drug_string = str(drug)
             drug_string = drug_string.replace("'","").replace("[","").replace("]","")
 
-            box1.insert(END, drug_string)
-            box2.insert(END, drug_string)
+            from_box.insert(tkinter.END, drug_string)
+            to_box.insert(tkinter.END, drug_string)
 
-        box1.bind("<<ListboxSelect>>", self.on_select_from)
-        box1.pack(side=LEFT, padx=5, pady=3)
+        from_box.bind("<<ListboxSelect>>", self.clicked_from)
+        from_box.pack(side=tkinter.LEFT, padx=5, pady=3)
 
-        box2.bind("<<ListboxSelect>>", self.on_select_to)
-        box2.pack(side=LEFT)
+        to_box.bind("<<ListboxSelect>>", self.clicked_to)
+        to_box.pack(side=tkinter.LEFT)
 
-        self.pack(fill=BOTH, expand=1, side=LEFT)
+        self.pack(fill=tkinter.BOTH, expand=1, side=tkinter.LEFT)
 
         self.dose_entry = ttk.Entry(self, width=35)
-        self.dose_entry.pack(side=TOP, anchor=W, padx=5, pady=3)
+        self.dose_entry.pack(side=tkinter.TOP, anchor=tkinter.W, padx=5, pady=3)
 
-        self.convert_from = StringVar()
+        self.convert_from = tkinter.StringVar()
         self.convert_from.set("mg of one antipsychotic roughly equates to:")
-        self.label_from = ttk.Label(self, anchor=W, textvariable=self.convert_from, width=35)
-        self.label_from.pack(side=TOP, anchor=W, padx=5, pady=3)
+        self.label_from = ttk.Label(self, anchor=tkinter.W, textvariable=self.convert_from, width=35)
+        self.label_from.pack(side=tkinter.TOP, anchor=tkinter.W, padx=5, pady=3)
 
-        self.result_given = StringVar()
+        self.result_given = tkinter.StringVar()
         self.result_given.set("?")
-        self.result_label = ttk.Label(self, anchor=W, textvariable=self.result_given, width=35, relief=RAISED, background="white")
-        self.result_label.pack(side=TOP, anchor=W, padx=5, pady=3)
+        self.result_label = ttk.Label(self, anchor=tkinter.W, textvariable=self.result_given, width=35, relief=tkinter.RAISED, background="white")
+        self.result_label.pack(side=tkinter.TOP, anchor=tkinter.W, padx=5, pady=3)
 
-        self.convert_to = StringVar()
+        self.convert_to = tkinter.StringVar()
         self.convert_to.set("mg of another antipsychotic.")
-        self.label_to = ttk.Label(self, anchor=W, textvariable=self.convert_to)
-        self.label_to.pack(side=TOP, anchor=W, padx=5, pady=3)
+        self.label_to = ttk.Label(self, anchor=tkinter.W, textvariable=self.convert_to)
+        self.label_to.pack(side=tkinter.TOP, anchor=tkinter.W, padx=5, pady=3)
 
-        frame = ttk.Frame(self, relief=FLAT, borderwidth=1)
-        frame.pack(fill=BOTH, expand=1)
+        frame = ttk.Frame(self, relief=tkinter.FLAT, borderwidth=1)
+        frame.pack(fill=tkinter.BOTH, expand=1)
 
         convert_button = ttk.Button(self, text="Convert", command = self.convert)
-        convert_button.pack(side=LEFT, padx=5, pady=3)
+        convert_button.pack(side=tkinter.LEFT, padx=5, pady=3)
 
         exit_button = ttk.Button(self, text="Exit", command = self.kill)
-        exit_button.pack(side=LEFT, padx=5, pady=3)
+        exit_button.pack(side=tkinter.LEFT, padx=5, pady=3)
 
     def give_result(self, result):
         toplevel = Toplevel()
@@ -123,16 +114,15 @@ class TransmuteAntipsychotics(ttk.Frame):
             self.result_given.set(result)
         except:
             self.result_given.set("Enter a numeric dose above.")
-#        self.give_result(result)
 
-    def on_select_from(self, val):
+    def clicked_from(self, val):
         sender = val.widget
         idx = sender.curselection()
         global from_drug 
         from_drug = sender.get(idx)
         self.convert_from.set("mg of "+from_drug+" roughly equates to:")
 
-    def on_select_to(self, val):
+    def clicked_to(self, val):
         sender = val.widget
         idx = sender.curselection()
         global to_drug
@@ -143,7 +133,7 @@ class TransmuteAntipsychotics(ttk.Frame):
         self.quit()
 
 def main():
-    root = Tk()
+    root = tkinter.Tk()
     app = TransmuteAntipsychotics(root)
     root.mainloop()
 
